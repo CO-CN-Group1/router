fff50541b6ddc1f607c5946092cde5c555ef6622
xtx <18822300009@163.com>
2019-10-28 01:49:19 +0800

    add router.tb

--- thinpad_top.srcs/sources_1/new/routing_table.v
+++ thinpad_top.srcs/sources_1/new/routing_table.v
@@ -6,22 +6,22 @@ module routing_table #(
     parameter BYTE_WIDTH = 8
 )
 (
-    input wire                      clk,
-    input wire                      rst,
-    input wire [IP_LENGTH-1:0]      dest_ip,
-    input wire                      dest_ip_valid,
-    output reg [IP_LENGTH-1:0]      nexthop,
-    output reg                      lookup_ready,
-    output reg                      nexthop_valid,
-    output reg                      nexthop_not_found,
+    input wire                          clk,
+    input wire                          rst,
+    input wire [IP_LENGTH-1:0]          dest_ip,
+    input wire                          dest_ip_valid,
+    output reg [IP_LENGTH-1:0]          nexthop,
+    output reg                          lookup_ready,
+    output reg                          nexthop_valid,
+    output reg                          nexthop_not_found,
 
-    input wire os_clk,
-    input wire[TRIE_INDEX_WIDTH-1:0] os_addr,
-    input wire[IP_LENGTH-1:0] os_din,
-    output wire[IP_LENGTH-1:0] os_dout,
-    input wire[IP_LENGTH/BYTE_WIDTH-1:0] os_wea,
-    input wire os_rst,
-    input wire os_en
+    input wire                          os_clk,
+    input wire[TRIE_INDEX_WIDTH-1:0]    os_addr,
+    input wire[IP_LENGTH+2*TRIE_INDEX_WIDTH-1:0] os_din,
+    output wire[IP_LENGTH+2*TRIE_INDEX_WIDTH-1:0]os_dout,
+    input wire                          os_wea,
+    input wire                          os_rst,
+    input wire                          os_en
 );
 
 reg[TRIE_INDEX_WIDTH-1:0] current = 1;
@@ -30,15 +30,15 @@ wire[IP_LENGTH+2*TRIE_INDEX_WIDTH-1:0] lookup_dout;
 xpm_memory_tdpram #(
     .ADDR_WIDTH_A(TRIE_INDEX_WIDTH),
     .WRITE_DATA_WIDTH_A(IP_LENGTH+2*TRIE_INDEX_WIDTH),
-    .BYTE_WRITE_WIDTH_A(BYTE_WIDTH),
+    .BYTE_WRITE_WIDTH_A(IP_LENGTH+2*TRIE_INDEX_WIDTH),
     .READ_DATA_WIDTH_A(IP_LENGTH+2*TRIE_INDEX_WIDTH),
     .ADDR_WIDTH_B(TRIE_INDEX_WIDTH),
-    .READ_LATENCY_A(1),
+    .READ_LATENCY_A(0),
     .WRITE_DATA_WIDTH_B(IP_LENGTH+2*TRIE_INDEX_WIDTH),
     .BYTE_WRITE_WIDTH_B(IP_LENGTH+2*TRIE_INDEX_WIDTH),
     .READ_DATA_WIDTH_B(IP_LENGTH+2*TRIE_INDEX_WIDTH),
-    .READ_LATENCY_B(1),
-    .MEMORY_SIZE(TRIE_INDEX_COUNT * (IP_LENGTH + 2*TRIE_INDEX_WIDTH)),
+    .READ_LATENCY_B(0),
+    .MEMORY_SIZE(TRIE_INDEX_COUNT * (IP_LENGTH + 2*TRIE_INDEX_WIDTH))
 ) xpm_memory_tdpram_data (
     .dina(os_din),
     .addra(os_addr),
@@ -48,7 +48,7 @@ xpm_memory_tdpram #(
     .rsta(os_rst),
     .ena(os_en),
 
-    .dinb(0),
+    .dinb(54'b0),
     .addrb(current),
     .web(1'b0),
     .doutb(lookup_dout),
@@ -64,6 +64,9 @@ localparam[2:0]
     STATE_IDLE = 1,
     STATE_SEARCH = 2;
 reg [2:0] state = STATE_INIT;
+
+
+reg [IP_LENGTH-1:0] best_ans;
 
 always @(posedge clk)begin
     if(rst)begin
@@ -82,8 +85,9 @@ always @(posedge clk)begin
     end
 end
 
-reg [IP_LENGTH-1:0] best_ans;
-reg [IP_LENGTH-1:0] pos;
+integer pos;
+
+reg[53:0] debug;
 
 always @(posedge clk) begin
     if(!rst && state!=STATE_INIT) begin
@@ -95,8 +99,10 @@ always @(posedge clk) begin
                     current <= 1;
                     best_ans <= 0;
                     pos <= 31;
+                    lookup_ready <= 0;
                 end
                 nexthop_valid <= 0;
+                debug <= 0;
                 nexthop_not_found <= 0;
             end
             STATE_SEARCH:begin
@@ -108,21 +114,21 @@ always @(posedge clk) begin
                         nexthop_not_found <= 0;
                     nexthop_valid <= 1;
                     state <= STATE_IDLE;
+                    lookup_ready <= 1;
                 end else begin
                     if(lookup_dout[IP_LENGTH-1:0]!=0)
                         best_ans <= lookup_dout[IP_LENGTH-1:0];
+                    if(pos!=-1)begin
                     if(dest_ip_cache[pos] == 0)begin
-                        current <= lookup_dout[IP_LENGTH+2*TRIE_INDEX_WIDTH-1:IP_LENGTH+TRIE_INDEX_WIDTH]; 
-                        if(pos==0)begin
-                            current <= 0;
-                        end else 
-                            pos<=pos-1;
+                        debug <= lookup_dout;
+                        current <= lookup_dout[IP_LENGTH+2*TRIE_INDEX_WIDTH-1:IP_LENGTH+TRIE_INDEX_WIDTH];
+                        pos<=pos-1;
                     end else begin
                         current <= lookup_dout[IP_LENGTH+TRIE_INDEX_WIDTH-1:IP_LENGTH];
-                        if(pos==0)begin
-                            current <= 0;
-                        end else 
-                            pos<=pos-1;
+                        pos<=pos-1;
+                    end
+                    end else begin
+                        current <= 0;
                     end
                 end
             end
