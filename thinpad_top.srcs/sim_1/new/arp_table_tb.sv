7fd4930117a51f646a81de9fb82e6a866ef5b852
xtx <18822300009@163.com>
2019-10-27 18:27:52 +0800

    add arp_table(ram)

---
+++ thinpad_top.srcs/sim_1/new/arp_table_tb.sv
@@ -0,0 +1,133 @@
+`timescale 1ns / 1ps
+
+module arp_table_tb;
+
+wire clk_50M, clk_11M0592, clk_125M, clk_125M_90deg;
+logic rst = 0;
+
+clock osc(
+    .clk_11M0592   (clk_11M0592),
+    .clk_50M       (clk_50M),
+    .clk_125M      (clk_125M),
+    .clk_125M_90deg(clk_125M_90deg)
+);
+
+logic[31:0] lookup_ip = 0;
+logic[47:0] lookup_mac;
+logic[15:0] lookup_port;
+logic lookup_valid = 0;
+logic lookup_mac_valid;
+logic lookup_mac_not_found;
+logic lookup_ready;
+
+logic[31:0] insert_ip = 0;
+logic[47:0] insert_mac = 0;
+logic[15:0] insert_port = 0;
+logic insert_valid = 0;
+logic insert_ready;       
+
+
+arp_table arp_table_inst(
+    .clk(clk_50M),
+    .rst(rst),
+    .lookup_ip(lookup_ip),
+    .lookup_mac(lookup_mac),
+    .lookup_port(lookup_port),
+    .lookup_valid(lookup_valid),
+    .lookup_mac_valid(lookup_mac_valid),
+    .lookup_mac_not_found(lookup_mac_not_found),
+    .lookup_ready(lookup_ready),
+
+    .insert_ip(insert_ip),
+    .insert_mac(insert_mac),
+    .insert_port(insert_port),
+    .insert_valid(insert_valid),
+    .insert_ready(insert_ready)        
+);
+
+logic init = 0;
+integer arp_table_fd,count_table = 0,arp_sample_in_fd,arp_sample_out_fd,count_ip = 0;
+logic[32+48+16-1:0] arp_table_data[0:31];
+logic[31:0] ip_data[0:9];
+initial begin
+    arp_table_fd = $fopen("arp_table_file.mem","r");
+    while(!$feof(arp_table_fd))begin 
+        $fscanf(arp_table_fd,"%24h",arp_table_data[count_table]);
+        $display("entry: %08h %12h %04h",arp_table_data[count_table][32+48+16-1:48+16],arp_table_data[count_table][48+16-1:16],arp_table_data[count_table][16-1:0]);
+        count_table +=1;
+    end;
+    $fclose(arp_table_fd);
+    arp_sample_in_fd = $fopen("arp_sample_in.mem","r");
+    while(!$feof(arp_sample_in_fd))begin 
+        $fscanf(arp_sample_in_fd,"%08h",ip_data[count_ip]);
+        $display("ip: %08h",ip_data[count_ip]);
+        count_ip +=1;
+    end;
+    $fclose(arp_sample_in_fd);
+    arp_sample_out_fd = $fopen("arp_sample_out.mem","w");
+    
+    rst = 1;
+    #100 rst = 0;
+    #100 init = 1;
+end
+
+logic[1:0] table_insert = 0;
+integer current = 0;
+
+always @(posedge clk_50M)begin
+    if(init)begin
+        if(table_insert==0)begin
+            
+            if(insert_ready&&!insert_valid)begin
+                insert_valid = 1;
+                {insert_ip,insert_mac,insert_port} = arp_table_data[current];
+                if(current==count_table-1)begin
+                    table_insert = 1;
+                    current = 0;
+                end else begin
+                    current = current + 1;
+                end
+            end else begin
+                lookup_valid = 0;
+                insert_valid = 0;    
+            end
+        end else if(table_insert==1)begin
+            
+            if(lookup_mac_valid)begin
+                $fwrite(arp_sample_out_fd,"%08h ",lookup_ip);
+                if(lookup_mac_not_found)begin
+                    $fwrite(arp_sample_out_fd,"not found\n");
+                end else begin
+                    $fwrite(arp_sample_out_fd,"%12h %04h\n",lookup_mac,lookup_port);
+                end
+            end else if(lookup_ready && !lookup_valid && !insert_valid)begin
+                lookup_ip = ip_data[current];
+                lookup_valid = 1;
+                if(current == count_ip-1)begin 
+                    table_insert = 2;
+                    
+                end else 
+                    current = current + 1;
+            end else begin
+                insert_valid = 0;
+                lookup_valid = 0;
+            end
+        end else begin
+            if(lookup_mac_valid)begin
+                $fwrite(arp_sample_out_fd,"%08h ",lookup_ip);
+                if(lookup_mac_not_found)begin
+                    $fwrite(arp_sample_out_fd,"not found\n");
+                end else begin
+                    $fwrite(arp_sample_out_fd,"%12h %04h\n",lookup_mac,lookup_port);
+                end
+                $fclose(arp_sample_out_fd);
+            end
+            insert_valid = 0;
+            lookup_valid = 0;
+        end
+    end
+end
+
+
+
+endmodule
--- thinpad_top.srcs/sources_1/new/arp_table.v
+++ thinpad_top.srcs/sources_1/new/arp_table.v
@@ -2,8 +2,10 @@ module arp_table #(
     parameter PORT_LENGTH = 16,
     parameter IP_LENGTH = 32,
     parameter MAC_LENGTH = 48,
-    parameter BUCKET_COUNT = 8,
-    parameter BUCKET_DEPTH = 8
+    parameter BUCKET_INDEX_WIDTH = 4,
+    parameter BUCKET_DEPTH_WIDTH = 3,
+    parameter BUCKET_DEPTH_COUNT = 8,
+    parameter BUCKET_INDEX_COUNT = 16
 )(
     input wire                  clk,
     input wire                  rst,
@@ -23,64 +25,145 @@ module arp_table #(
     output reg                  insert_ready                 
 );
 
-reg [IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:0]  data_entry[0:BUCKET_COUNT-1][0:BUCKET_DEPTH-1];
-
-integer bucket_depth[0:BUCKET_COUNT-1];
-integer bucket_ins_pos[0:BUCKET_COUNT-1];
-
-
 localparam[2:0]
-    STATE_IDLE = 0,
-    STATE_SEARCH = 1,
-    STATE_INSERT = 2;
+    STATE_INIT = 0,
+    STATE_IDLE = 1,
+    STATE_SEARCH = 2,
+    STATE_INSERT1 = 3,
+    STATE_INSERT2 = 4;
 
-reg[2:0] state = STATE_IDLE;
-
+reg[2:0] state = STATE_INIT;
 
 reg[IP_LENGTH-1:0] lookup_ip_cache;
-reg[2:0] lookup_bucket_index;
-integer current = 0;
 
-integer debug = 0;
+wire [BUCKET_INDEX_WIDTH-1:0] lookup_bucket_index;
+assign lookup_bucket_index = {lookup_ip_cache[11],lookup_ip_cache[7],lookup_ip_cache[5],lookup_ip_cache[2]};
 
-initial begin
-    lookup_ready <= 1;
-    insert_ready <= 1; 
-    lookup_mac_valid <= 0;
-end
+reg [BUCKET_DEPTH_WIDTH-1:0] lookup_current_depth;
 
-always @(posedge clk) begin
-    if(rst) begin
+reg  [IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:0] lookup_din;
+wire [IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:0] lookup_dout;
+wire [BUCKET_INDEX_WIDTH+BUCKET_DEPTH_WIDTH-1:0] lookup_addr;
+
+assign lookup_addr = {lookup_bucket_index,lookup_current_depth};
+
+
+reg [IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:0] lookup_data_entry;
+
+
+wire [BUCKET_INDEX_WIDTH-1:0] insert_bucket_index;
+assign insert_bucket_index = {insert_data_entry[11+MAC_LENGTH+PORT_LENGTH],insert_data_entry[7+MAC_LENGTH+PORT_LENGTH],insert_data_entry[5+MAC_LENGTH+PORT_LENGTH],insert_data_entry[2+MAC_LENGTH+PORT_LENGTH]};
+reg  [BUCKET_DEPTH_WIDTH-1:0] insert_current_depth;
+reg  [BUCKET_DEPTH_WIDTH-1:0] insert_hash_depth;
+
+reg  [IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:0] insert_din;
+wire [IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:0] insert_dout;
+wire [BUCKET_INDEX_WIDTH+BUCKET_DEPTH_WIDTH-1:0] insert_addr;
+reg  insert_enable;
+
+assign insert_addr = {insert_bucket_index,insert_current_depth};
+
+reg [IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:0] insert_data_entry;
+
+xpm_memory_tdpram #(
+    .ADDR_WIDTH_A(BUCKET_INDEX_WIDTH+BUCKET_DEPTH_WIDTH),
+    .WRITE_DATA_WIDTH_A(IP_LENGTH+MAC_LENGTH+PORT_LENGTH),
+    .BYTE_WRITE_WIDTH_A(IP_LENGTH+MAC_LENGTH+PORT_LENGTH),
+
+    .ADDR_WIDTH_B(BUCKET_INDEX_WIDTH+BUCKET_DEPTH_WIDTH),
+    .WRITE_DATA_WIDTH_B(IP_LENGTH+MAC_LENGTH+PORT_LENGTH),
+    .BYTE_WRITE_WIDTH_B(IP_LENGTH+MAC_LENGTH+PORT_LENGTH),
+    .MEMORY_SIZE(BUCKET_INDEX_COUNT*BUCKET_DEPTH_COUNT*(IP_LENGTH+MAC_LENGTH+PORT_LENGTH)),
+    .READ_LATENCY_A(1),
+    .READ_LATENCY_B(0)
+) xpm_memory_tdpram_inst (
+    .dina(lookup_din),
+    .douta(lookup_dout),
+    .addra(lookup_addr),
+    .wea(1'b0),
+    .clka(clk),
+    .rsta(rst),
+    .ena(1'b1),
+
+    .dinb(insert_din),
+    .doutb(insert_dout),
+    .addrb(insert_addr),
+    .web(insert_enable),
+    .clkb(clk),
+    .rstb(rst),
+    .enb(1'b1)
+);
+
+always @(posedge clk)begin
+    if(rst)begin
+        lookup_ready <= 0;
+        insert_ready <= 0;
         lookup_mac <= 0;
         lookup_port <= 0;
         lookup_mac_valid <= 0;
-        lookup_mac_not_found <= 0;
-        lookup_ready <= 1;
-        state <= STATE_IDLE;
+        lookup_mac_not_found <=0;
         lookup_ip_cache <= 0;
-        lookup_bucket_index <=0;
-    end else begin
+        lookup_current_depth <= 0;
+        insert_enable <= 0;
+        state <= STATE_INIT;
+        lookup_data_entry <= 0;
+        insert_data_entry <= 0;
+        insert_current_depth <= 0;
+        insert_enable <= 0;
+        insert_hash_depth <= 0;
+    end else if(state == STATE_INIT)begin
+        lookup_ready <= 1;
+        insert_ready <= 1;
+        lookup_mac <= 0;
+        lookup_port <= 0;
+        lookup_mac_valid <= 0;
+        lookup_mac_not_found <=0;
+        lookup_ip_cache <= 0;
+        lookup_current_depth <= 0;
+        insert_enable <= 0;
+        state <= STATE_IDLE;
+        lookup_data_entry <= 0;
+        insert_data_entry <= 0;
+        insert_current_depth <= 0;
+        insert_enable <= 0;
+        insert_hash_depth <= 0;
+        state <= STATE_IDLE;
+    end
+end
+
+always @(posedge clk) begin
+    if(!rst && state!=STATE_INIT) begin
         case(state)
             STATE_IDLE:begin
-                
                 lookup_mac_valid <= 0;
                 lookup_mac_not_found <= 0;
                 if(lookup_valid) begin
-                    debug <= 1;
+                    lookup_current_depth <= 0;
                     lookup_ip_cache <= lookup_ip;
-                    lookup_bucket_index <= lookup_ip[IP_LENGTH-1: IP_LENGTH-3];
                     lookup_ready <= 0;
                     insert_ready <= 0;
-                    current <= 0;
+                    lookup_mac <= 0;
+                    lookup_port <= 0;
                     state <= STATE_SEARCH;  
                 end
             end
-            STATE_INSERT:begin
+            STATE_INSERT1:begin
+                lookup_mac_valid <= 0;
+                lookup_mac_not_found <= 0;
+            end
+            STATE_INSERT2:begin
                 lookup_mac_valid <= 0;
                 lookup_mac_not_found <= 0;
             end
             STATE_SEARCH:begin
-                if(current == bucket_depth[lookup_bucket_index])begin
+                if(lookup_dout[IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:MAC_LENGTH+PORT_LENGTH] == lookup_ip_cache) begin
+                    lookup_mac_valid <= 1;
+                    lookup_mac_not_found <= 0;
+                    {lookup_mac,lookup_port} <= lookup_dout[MAC_LENGTH + PORT_LENGTH-1:0];
+                    lookup_ready <= 1;
+                    insert_ready <= 1;
+                    state <= STATE_IDLE;
+                end else if(lookup_current_depth == BUCKET_DEPTH_COUNT-1)begin
                     state <= STATE_IDLE;
                     lookup_mac <= 0;
                     lookup_mac_valid <= 1;
@@ -89,60 +172,55 @@ always @(posedge clk) begin
                     lookup_ready <= 1;
                     insert_ready <= 1;
                 end else begin
-                    if(data_entry[lookup_bucket_index][current][IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:MAC_LENGTH+PORT_LENGTH]==lookup_ip_cache)begin
-                        state<=STATE_IDLE;
-                        lookup_ready<=1;
-                        insert_ready<=1;
-                        lookup_mac_valid <=1;
-                        lookup_mac_not_found <= 0;
-                        {lookup_mac,lookup_port} <= data_entry[lookup_bucket_index][current][MAC_LENGTH + PORT_LENGTH-1:0];
-                    end else begin
-                        current <= current+1;
-                    end
+                    lookup_current_depth <= lookup_current_depth + 1;
                 end
             end
         endcase
     end
 end
-
-reg[IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:0] data_entry_ins;
-reg insert_bucket_index;
 
 always @(posedge clk)begin
-    if(rst) begin
-        insert_ready <= 1;
-        state<= STATE_IDLE;
-        data_entry_ins <= 0;
-        insert_bucket_index <= 0;
-    end else begin
+    if(!rst && state!=STATE_INIT) begin
         case(state)
             STATE_IDLE:begin
-                if(insert_valid)begin
-                    state <= STATE_INSERT;
+                if(insert_enable)begin
+                    insert_enable <= 0;
+                    insert_hash_depth <= insert_hash_depth + 3;
+                end
+                if(insert_valid&&!lookup_valid)begin
+                    state <= STATE_INSERT1;
                     insert_ready <= 0;
                     lookup_ready <= 0;
-                    insert_bucket_index <= insert_ip[IP_LENGTH-1:IP_LENGTH-3];
-                    data_entry_ins <= {insert_ip,insert_mac,insert_port};
+                    insert_current_depth <= 0;
+                    insert_data_entry <= {insert_ip,insert_mac,insert_port};
                 end
             end
-            STATE_INSERT:begin
+            STATE_INSERT1:begin
+                if(insert_dout[IP_LENGTH+MAC_LENGTH+PORT_LENGTH-1:MAC_LENGTH+PORT_LENGTH] == insert_data_entry)begin
+                    insert_hash_depth <= insert_current_depth;
+                    insert_din <= insert_data_entry;
+                    insert_enable <= 1;
+                    state <= STATE_INSERT2; 
+                end else if(insert_current_depth == BUCKET_DEPTH_COUNT-1)begin 
+                    insert_current_depth <= insert_hash_depth;
+                    insert_din <= insert_data_entry;
+                    insert_enable <= 1;
+                    state <= STATE_INSERT2;
+                end else
+                    insert_current_depth <= insert_current_depth + 1; 
+            end
+            STATE_INSERT2:begin
+                insert_enable <= 0;
+                insert_hash_depth <= insert_hash_depth + 3;
                 state <= STATE_IDLE;
-                insert_ready  <= 1;
+                insert_ready <= 1;
                 lookup_ready <= 1;
-                data_entry[insert_bucket_index][bucket_ins_pos[insert_bucket_index]] <= data_entry_ins;
-                if(bucket_ins_pos[insert_bucket_index]==BUCKET_DEPTH)
-                    bucket_ins_pos[insert_bucket_index] <= 0;
-                else
-                    bucket_ins_pos[insert_bucket_index] <= bucket_ins_pos[insert_bucket_index] + 1;
-
-                if(bucket_depth[insert_bucket_index]<BUCKET_DEPTH)
-                    bucket_depth[insert_bucket_index] <= bucket_depth[insert_bucket_index]+1;
             end
             STATE_SEARCH:begin
+                insert_data_entry <= 0;
             end
         endcase
     end
 end
-
 
 endmodule
