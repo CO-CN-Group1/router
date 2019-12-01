0c47359ac2944c33ffd08ad29bb65f305008e0ab
FactorialN <lgz17@mails.tsinghua.edu.cn>
2019-12-02 05:07:06 +0800

    trying to fix CI/CD problem

--- thinpad_top.srcs/sources_1/ip/eth_mac/eth_mac_stub.vhdl
+++ thinpad_top.srcs/sources_1/ip/eth_mac/eth_mac_stub.vhdl
@@ -1,10 +1,10 @@
 -- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
--- Date        : Mon Dec  2 00:22:30 2019
--- Host        : Spectre running 64-bit major release  (build 9200)
+-- Date        : Mon Dec  2 04:48:07 2019
+-- Host        : DESKTOP-RN0QBT2 running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
---               C:/codes/router/cod19grp1/thinpad_top.srcs/sources_1/ip/eth_mac/eth_mac_stub.vhdl
+--               C:/Users/FactorialN/Downloads/wow/cod19grp1/thinpad_top.srcs/sources_1/ip/eth_mac/eth_mac_stub.vhdl
 -- Design      : eth_mac
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xc7a100tfgg676-2L
--- thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.vhdl
+++ thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.vhdl
@@ -1,10 +1,10 @@
 -- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
--- Date        : Mon Dec  2 00:22:11 2019
--- Host        : Spectre running 64-bit major release  (build 9200)
+-- Date        : Mon Dec  2 04:47:48 2019
+-- Host        : DESKTOP-RN0QBT2 running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode funcsim
---               C:/codes/router/cod19grp1/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.vhdl
+--               C:/Users/FactorialN/Downloads/wow/cod19grp1/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_sim_netlist.vhdl
 -- Design      : pll_example
 -- Purpose     : This VHDL netlist is a functional simulation representation of the design and should not be modified or
 --               synthesized. This netlist cannot be used for SDF annotated simulation.
