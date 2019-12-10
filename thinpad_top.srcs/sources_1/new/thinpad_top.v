fc26270a580d020142d8002cb074902d5a62500f
xtx <18822300009@163.com>
2019-12-10 23:07:45 +0800

    maybe fixed a bug

--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -86,12 +86,12 @@ module thinpad_top(
 /* =========== Demo code begin =========== */
 
 // PLL分频示例
-wire locked, clk_10M, clk_40M, clk_125M, clk_200M;
+wire locked, clk_10M, clk_20M, clk_125M, clk_200M;
 pll_example clock_gen 
  (
   // Clock out ports
   .clk_out1(clk_10M), // 时钟输出1，频率在IP配置界面中设�?
-  .clk_out2(clk_40M), // 时钟输出2，频率在IP配置界面中设�?
+  .clk_out2(clk_20M), // 时钟输出2，频率在IP配置界面中设�?
   .clk_out3(clk_125M), // 时钟输出3，频率在IP配置界面中设�?
   .clk_out4(clk_200M), // 时钟输出4，频率在IP配置界面中设�?
   // Status and control signals
@@ -465,7 +465,7 @@ assign int = {5'b00000, timer_int};
 assign uart_wrn = uart_we_o;
 assign uart_rdn = uart_re_o;
 mips_cpu mips_cpu_inst(
-    .clk(clk_40M),
+    .clk(clk_20M),
     .rst(~locked),
     .int_i(int),
 
